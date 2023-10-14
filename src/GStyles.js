bcd179ac7878339f1d988fb58d304e2af9494e3e
Hakan Ekici <hakan.ekici@basarsoft.com.tr>
2023-10-14 13:18:29 +0300

    first commit

---
+++ src/GStyles.js
@@ -0,0 +1,10 @@
+import {Dimensions, Platform} from 'react-native';
+
+const {height, width} = Dimensions.get('window');
+
+export default {
+  colors: {},
+  spacings: {},
+  sizes: {},
+  fontSizes: {},
+};
---
+++ src/screens/Auth/SignUp/SignUp.style.js
@@ -0,0 +1,9 @@
+import {Dimensions, StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 1,
+  },
+});
